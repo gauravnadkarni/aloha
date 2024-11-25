cfac1677553e8a1f1c0cd706454874856b07a55d
Gaurav <nadkarnigaurav@gmail.com>
2024-11-25 20:36:06 +0530

    Fixed issues in test

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,17 @@ import type {Config} from "jest";
 const config: Config = {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": [
+      "ts-jest",
+      {
+        isolatedModules: true, // Makes TypeScript files work faster
+        tsconfig: "tsconfig.json", // Path to your TypeScript config file
+      },
+    ],
   },
   moduleNameMapper: {
     "\\.(css|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
+    "\\.(jpg|jpeg|png|gif)$": "<rootDir>/__mocks__/fileMock.js", // Mock image files
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Setup for jest-dom
   testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore these folders
